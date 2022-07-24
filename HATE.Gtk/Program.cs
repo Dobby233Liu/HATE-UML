0dc123fba371dca983fa0c15caf5377a64f2e79a
Liu Wenyuan <15816141883@163.com>
2022-07-24 18:29:56 +0800

    try 2

---
+++ HATE.Gtk/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Eto.Forms;
+using HATE;
+
+namespace HATE.Gtk
+{
+	class Program
+	{
+		/// <summary>
+		/// The main entry point for the application
+		/// </summary>
+		[STAThread]
+		static void Main()
+		{
+			new Application(Eto.Platforms.Gtk).Run(new HATE.MainForm());
+		}
+	}
+}
